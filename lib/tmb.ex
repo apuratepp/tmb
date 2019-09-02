1072b0bdf9822617dbc906fe8690fa465b2b9e61
Josep <apuratepp@gmail.com>
2019-09-02 14:11:04 +0200

    initial commit

---
+++ lib/tmb.ex
@@ -0,0 +1,18 @@
+defmodule Tmb do
+  @moduledoc """
+  Documentation for Tmb.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tmb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
